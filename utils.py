c41f1c6159e33692fbba269379f336b3cd385c77
JakeYoo87 <jh0113.yoo@gmail.com>
2021-05-17 23:02:57 +0900

    [0517] code refactor

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 import torch
 from pycls.models.nas.nas import Cell
 
+
 class DropChannel(torch.nn.Module):
     def __init__(self, p, mod):
         super(DropChannel, self).__init__()
         self.mod = mod
         self.p = p
+
     def forward(self, s0, s1, droppath):
         ret = self.mod(s0, s1, droppath)
         return ret
@@ -15,6 +17,7 @@ class DropConnect(torch.nn.Module):
     def __init__(self, p):
         super(DropConnect, self).__init__()
         self.p = p
+
     def forward(self, inputs):
         batch_size = inputs.shape[0]
         dim1 = inputs.shape[2]
@@ -26,10 +29,11 @@ class DropConnect(torch.nn.Module):
         random_tensor += torch.rand([batch_size, channel_size, 1, 1], dtype=inputs.dtype, device=inputs.device)
         binary_tensor = torch.floor(random_tensor)
         output = inputs / keep_prob * binary_tensor
-        return output    
+        return output
+
 
 def add_dropout(network, p, prefix=''):
-    #p = 0.5
+    # p = 0.5
     for attr_str in dir(network):
         target_attr = getattr(network, attr_str)
         if isinstance(target_attr, torch.nn.Conv2d):
@@ -37,43 +41,45 @@ def add_dropout(network, p, prefix=''):
         elif isinstance(target_attr, Cell):
             setattr(network, attr_str, DropChannel(p, target_attr))
     for n, ch in list(network.named_children()):
-        #print(f'{prefix}add_dropout {n}')
+        # print(f'{prefix}add_dropout {n}')
         if isinstance(ch, torch.nn.Conv2d):
             setattr(network, n, torch.nn.Sequential(ch, DropConnect(p)))
         elif isinstance(ch, Cell):
             setattr(network, n, DropChannel(p, ch))
         else:
             add_dropout(ch, p, prefix + '\t')
-             
-
 
 
 def orth_init(m):
     if isinstance(m, (torch.nn.Conv2d, torch.nn.Linear)):
         torch.nn.init.orthogonal_(m.weight)
 
+
 def uni_init(m):
     if isinstance(m, (torch.nn.Conv2d, torch.nn.Linear)):
         torch.nn.init.uniform_(m.weight)
+
 
 def uni2_init(m):
     if isinstance(m, (torch.nn.Conv2d, torch.nn.Linear)):
         torch.nn.init.uniform_(m.weight, -1., 1.)
 
+
 def uni3_init(m):
     if isinstance(m, (torch.nn.Conv2d, torch.nn.Linear)):
         torch.nn.init.uniform_(m.weight, -.5, .5)
 
+
 def norm_init(m):
     if isinstance(m, (torch.nn.Conv2d, torch.nn.Linear)):
         torch.nn.init.norm_(m.weight)
+
 
 def eye_init(m):
     if isinstance(m, torch.nn.Linear):
         torch.nn.init.eye_(m.weight)
     elif isinstance(m, torch.nn.Conv2d):
         torch.nn.init.dirac_(m.weight)
-
 
 
 def fixup_init(m):
